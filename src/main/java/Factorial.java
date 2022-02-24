38fd9395a80a1826b41b8a0df8beecb3aafdfc59
jujosa09 <jujosadols@uma.es>
2022-02-24 12:28:28 +0100

    Actualizado Factorial y FactorialTest

--- src/main/java/Factorial.java
+++ src/main/java/Factorial.java
@@ -1,5 +1,11 @@
 public class Factorial {
     public int compute (int value){
-        return 1;
+        int result;
+        if((value == 0) || (value == 1)){
+            result = 1;
+        }else{
+            result = 2;
+        }
+        return result;
     }
 }
--- src/test/java/FactorialTest.java
+++ src/test/java/FactorialTest.java
@@ -4,10 +4,26 @@ import static org.junit.Assert.assertEquals;
 
 class FactorialTest {
     @Test
-    public void test1(){
+    public void shouldComputeReturnOneIfTheNumberIsZero(){
         Factorial factorial = new Factorial();
         int expectedValue=1;
         int obtainedValue= factorial.compute(0);
+
+        assertEquals(expectedValue,obtainedValue);
+    }
+    @Test
+    public void shouldComputeReturnOneIfTheNumberIsOne(){
+        Factorial factorial = new Factorial();
+        int expectedValue=1;
+        int obtainedValue= factorial.compute(1);
+
+        assertEquals(expectedValue,obtainedValue);
+    }
+    @Test
+    public void shouldComputeReturnOneIfTheNumberIsTwo(){
+        Factorial factorial = new Factorial();
+        int expectedValue=2;
+        int obtainedValue= factorial.compute(2);
 
         assertEquals(expectedValue,obtainedValue);
     }
