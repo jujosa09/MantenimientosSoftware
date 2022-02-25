83b4e66392e8faf621f8a8047a1875a65a504c17
jujosa09 <jujosadols@uma.es>
2022-02-25 11:42:12 +0100

    Update Factorial

--- prFactorial/src/main/java/Factorial.java
+++ prFactorial/src/main/java/Factorial.java
@@ -6,10 +6,6 @@ public class Factorial {
         }
         if((value == 0) || (value == 1)){
             result = 1;
-        }else if (value == 2){
-            result = 2;
-        }else if(value == 3){
-            result = 6;
         }else{
             result = value * compute(value-1);
         }
