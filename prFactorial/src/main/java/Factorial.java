14fd2b8a2b1bde41d73504c24cb4b041d3f1b949
jujosa09 <jujosadols@uma.es>
2022-02-25 11:17:50 +0100

    Update
    Update Factorial y FactorialTest

--- prFactorial/src/main/java/Factorial.java
+++ prFactorial/src/main/java/Factorial.java
@@ -1,10 +1,17 @@
 public class Factorial {
     public int compute (int value){
         int result;
+        if(value < 0){
+            throw new RuntimeException("The value is negative: " + value);
+        }
         if((value == 0) || (value == 1)){
             result = 1;
-        }else{
+        }else if (value == 2){
             result = 2;
+        }else if(value == 3){
+            result = 6;
+        }else{
+            result = value * compute(value-1);
         }
         return result;
     }
--- prFactorial/src/test/java/FactorialTest.java
+++ prFactorial/src/test/java/FactorialTest.java
@@ -1,11 +1,12 @@
 import org.junit.jupiter.api.Test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThrows;
 
 class FactorialTest {
     @Test
     public void shouldComputeReturnOneIfTheNumberIsZero(){
-        Factorial factorial = new Factorial();
+        var factorial = new Factorial();
         int expectedValue=1;
         int obtainedValue= factorial.compute(0);
 
@@ -13,19 +14,40 @@ class FactorialTest {
     }
     @Test
     public void shouldComputeReturnOneIfTheNumberIsOne(){
-        Factorial factorial = new Factorial();
+        var factorial = new Factorial();
         int expectedValue=1;
         int obtainedValue= factorial.compute(1);
 
         assertEquals(expectedValue,obtainedValue);
     }
     @Test
-    public void shouldComputeReturnOneIfTheNumberIsTwo(){
-        Factorial factorial = new Factorial();
+    public void shouldComputeReturn2IfTheNumberIsTwo(){
+        var factorial = new Factorial();
         int expectedValue=2;
         int obtainedValue= factorial.compute(2);
 
         assertEquals(expectedValue,obtainedValue);
+    }
+    @Test
+    public void shouldComputeReturn6IfTheNumberIsThree(){
+        var factorial = new Factorial();
+        int expectedValue=6;
+        int obtainedValue= factorial.compute(3);
+
+        assertEquals(expectedValue,obtainedValue);
+    }
+    @Test
+    public void shouldComputeReturn720IfTheNumberIs6(){
+        var factorial = new Factorial();
+        int expectedValue=720;
+        int obtainedValue= factorial.compute(6);
+
+        assertEquals(expectedValue,obtainedValue);
+    }
+    @Test
+    public void shouldComputeReturnErrorIfTheNumberIsNegative(){
+        var factorial = new Factorial();
+        assertThrows(RuntimeException.class, () -> factorial.compute(-1 ));
     }
 
 }
