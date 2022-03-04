a4c238e8006e5de0d947b729b08525413e95fd02
jujosa09 <jujosadols@uma.es>
2022-03-04 11:59:20 +0100

    Añadido rama develop y actualizado clase Fibonacci y FibonacciTest

--- prFibonacci/src/main/java/Fibonacci.java
+++ prFibonacci/src/main/java/Fibonacci.java
@@ -1,2 +1,13 @@
 public class Fibonacci {
+    public int compute(int valor){
+        int resultado;
+        if(valor == 1){
+            resultado = 0;
+        }else if(valor == 2 || valor == 3){
+            resultado = 1;
+        }else{
+            throw new RuntimeException("El valor que has introducido es erroneo: " + valor);
+        }
+        return resultado;
+    }
 }
--- prFibonacci/src/test/java/FibonacciTest.java
+++ prFibonacci/src/test/java/FibonacciTest.java
@@ -1,8 +1,39 @@
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.Assert.assertThrows;
+
+/**
+ * Clase creada para probar la clase Fibonacci
+ *
+ * Valores de entradas que se van a probar:
+ *  Entrada:    Valor:
+ *      1   ->  0
+ *      2   ->  1
+ *      3   ->  1
+ *      4   ->  2
+ *      5   ->  3
+ *      6   ->  5
+ *      7   ->  8
+ */
 
 public class FibonacciTest {
+    private Fibonacci fibonacci;
+    @BeforeEach
+    public void setup(){
+        fibonacci = new Fibonacci();
+    }
     @Test
-    public void Test1(){
+    public void SiLaEntradaEs1LaSalidaEs0(){
+        int valorEsperado = 0;
+        int valorObtenido = fibonacci.compute(1);
 
+        assertEquals(valorEsperado,valorObtenido);
+    }
+    @Test
+    public void SiLaEntradaEs2LaSalidaEs1(){
+        int valorEsperado = 1;
+        int valorObtenido = fibonacci.compute(2);
+        assertEquals(valorEsperado,valorObtenido);
     }
 }
